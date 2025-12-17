54277db8e4416d54b14b21c5bea599095cbe99d5
Ian <ianpiggykirby@gmail.com>
2025-12-17 11:12:17 -0800

    Class

--- notes-10-classes.py
+++ notes-10-classes.py
@@ -35,6 +35,69 @@ class Pokemon:
         print(f"    Level: {self.level}")
         print("--------------------------------")
 
+    def dance(self):
+        """Make your pokemon dance"""
+        print(f"{self.name} is dancing")
+
+    def find_something(self, how_many_things=1) -> str:
+        """Send pokemon to find something
+
+        Returns:
+            a list of str representing what it found"""
+        things = [
+            "pinap berry",
+            "razz berry",
+            "nanab berry",
+            "golden razz berry",
+            "leftovers",
+            "moon stone",
+        ]
+        found_things = []
+
+        for _ in range(how_many_things):
+            found_things.append(random.choice(things))
+
+        return found_things
+
+
+class Squirtle(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.name = "Squirtle"
+        self.species = "Squirtle"
+        self.type = "water"
+        self.level = 1
+
+    def water_gun(self):
+        """Squirtle shoots water out of its mouth"""
+        print(f"{self.name} used water gun!")
+
+    def bubble_beam(self):
+        """Squirtle shoots bubble beam out of its mouth"""
+        print(f"{self.name} used bubble beam!")
+
+
+class Snorlax(Pokemon):
+    def __init__(self):
+        super().__init__()
+        self.name = "Snorlax"
+        self.species = "Snorlax"
+        self.type = "normal"
+        self.level = 5
+
+    def gluttony(self):
+        """Snorlax's hidden ability which allows it to eat held berries at half health"""
+        print(f"{self.name} used gluttony!")
+        print(f"{self.name} from half health heal into full health!")
+
+    def stats(self):
+        """Display the states of the pokemon"""
+        print(f"    Name: {self.name}")
+        print(f"    Type:  {self.type}")
+        print(f"    Level: {self.level}")
+        print(f"    Age:    {self.age}")
+        print("--------------------------------")
+
 
 if __name__ == "__main__":
     # Create a pokemon object
@@ -61,9 +124,30 @@ if __name__ == "__main__":
         for _ in range(100):
             Pokemon()
 
+        print(f"{pokemon_one.name} found these things: {pokemon_one.find_something()}")
+
         # Tell our pokemon to talk
         pokemon_one.talk()
         pokemon_two.talk()
         # Display stats of pokemon_one
         pokemon_one.stats()
         pokemon_two.stats()
+        # Make your pokemon dance
+        pokemon_one.dance()
+        pokemon_two.dance()
+
+        squirtle_one = Squirtle()
+        squirtle_one.stats()
+        # use .water_gun()
+        squirtle_one.water_gun()
+        # use bubble_beam()
+        squirtle_one.bubble_beam()
+        # use .talk()
+        squirtle_one.talk()
+
+        snorlax_one = Snorlax()
+        snorlax_one.stats()
+        # use .gluttony()
+        snorlax_one.gluttony()
+        # use .talk()
+        snorlax_one.talk()
